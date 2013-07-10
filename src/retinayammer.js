177c6490224e830873e86faa6540ffaa45c37417
Pedro Morais <pmorais@yammer-inc.com>
2013-07-10 16:07:18 +0100

    only upgrade the image sizes if the device has a retina display

--- src/retinayammer.js
+++ src/retinayammer.js
@@ -1,12 +1,14 @@
-chrome.webRequest.onBeforeRequest.addListener(function(details) {        
-        if (details.url.indexOf('#retina') > 0) {
-            return {}
-        } else {
-            var new_url = details.url.replace('16x16', '32x32').replace('33x33', '66x66').replace('48x48', '96x96').replace('75x75', '150x150');
-            console.log('Retinifying ' + details.url + ' to ' + new_url);
-            return { redirectUrl: new_url + '#retina' };
-        }
-    },
-    { urls: ["*://mug0.assets-yammer.com/mugshot/images/*"], types: ["image"] },
-    ["blocking"]
-);
+if (window.devicePixelRatio && window.devicePixelRatio > 1) {
+    chrome.webRequest.onBeforeRequest.addListener(function(details) {        
+            if (details.url.indexOf('#retina') > 0) {
+                return {}
+            } else {
+                var new_url = details.url.replace('16x16', '32x32').replace('33x33', '66x66').replace('48x48', '96x96').replace('75x75', '150x150');
+                console.log('Retinifying ' + details.url + ' to ' + new_url);
+                return { redirectUrl: new_url + '#retina' };
+            }
+        },
+        { urls: ["*://mug0.assets-yammer.com/mugshot/images/*"], types: ["image"] },
+        ["blocking"]
+    );
+}
